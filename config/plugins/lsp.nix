cb84f25453fdba0d706b66811fb75bb1c56a5121
Hendrik Roth <hi@hendrikroth.com>
2024-12-20 01:18:28 +0100

    lsp servers

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,5 +1,30 @@
 { pkgs, ... }: {
   plugins.lsp = {
     enable = true;
+    servers = {
+      ts_ls.enable = true;
+      cssls.enable = true;
+      tailwindcss.enable = true;
+      html.enable = true;
+      astro.enable = true;
+      svelte.enable = true;
+      vuels.enable = true;
+      pyright.enable = true;
+      marksman.enable = true;
+      nil_ls.enable = true;
+      dockerls.enable = true;
+      bashls.enable = true;
+      yamlls.enable = true;
+      gopls.enable = true;
+      lua_ls = {
+        enable = true;
+        settings.telemetry.enable = false;
+      };
+      rust_analyzer = {
+        enable = true;
+        installRustc = true;
+        installCargo = true;
+      };
+    };
   };
 }
