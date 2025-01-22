7b7086f57d899fd789c45422bb0bd1bd9b17b9d5
Hendrik Roth <hi@hendrikroth.com>
2025-01-22 16:12:48 +0100

    mermaid color settings

--- config/plugins/diagram.nix
+++ config/plugins/diagram.nix
@@ -2,5 +2,14 @@
 {
   plugins.diagram-nvim = {
     enable = true;
+
+    settings = {
+      renderer_options = {
+        mermaid = {
+          background = "transparent";
+          theme = "dark";
+        };
+      };
+    };
   };
 }
