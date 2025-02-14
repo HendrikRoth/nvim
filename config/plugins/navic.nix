6d40da10bae409c5efd8a99072ea227b05971fff
Hendrik Roth <hi@hendrikroth.com>
2025-02-14 17:17:40 +0100

    navic settings

--- config/plugins/navic.nix
+++ config/plugins/navic.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }: {
+{ ... }: {
   plugins.navic = {
     enable = true;
+
+    settings = {
+      lsp = {
+        auto_attach = true;
+        preference = [
+          "biome"
+          "tsserver"
+          "nixd"
+          "nix_ls"
+        ];
+      };
+    };
   };
 }
