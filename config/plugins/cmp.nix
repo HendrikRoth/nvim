128932f8a8fed7b3a3e484ff371f8610bbf42e20
Hendrik Roth <hi@hendrikroth.com>
2025-02-14 17:24:32 +0100

    blink settings

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -13,11 +13,18 @@
       };
 
       completion = {
-        documentation.auto_show = true;
+        accept.auto_brackets.enabled = true;
+        documentation = {
+          auto_show = true;
+          window.border = "rounded";
+        };
         ghost_text.enabled = true;
       };
 
-      signature.enabled = true;
+      signature = {
+        enabled = true;
+        window.border = "rounded";
+      };
 
       sources = {
         default = [
