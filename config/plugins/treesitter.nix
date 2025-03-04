a23691f3c651312f5ffd2ede7d6d811c009710e2
Hendrik Roth <hi@hendrikroth.com>
2025-03-04 23:09:11 +0100

    added treesitter providers

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -6,6 +6,22 @@
       hightlight.enable = true;
       nixGrammars.enable = true;
       indent.enable = true;
+      auto_install = true;
+      ensure_installed = [
+        "comment"
+        "css"
+        "go"
+        "html"
+        "javascript"
+        "jsonc"
+        "lua"
+        "markdown"
+        "scss"
+        "svelte"
+        "toml"
+        "typescript"
+        "yaml"
+      ];
     };
   };
 }
