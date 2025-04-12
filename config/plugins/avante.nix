4065737846469fd784770940e83bbdf4519d0017
Hendrik Roth <hi@hendrikroth.com>
2025-04-12 20:48:35 +0200

    other ai model

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -10,7 +10,7 @@
           __inherited_from = "openai";
           endpoint = "https://openrouter.ai/api/v1";
           api_key_name = "OPENROUTER_API_KEY";
-          model = "google/gemini-2.0-flash-lite-preview-02-05:free";
+          model = "google/gemini-2.0-flash-exp:free";
         };
       };
     };
